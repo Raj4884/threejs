9d724055092decd8c34002ad3d4ab81ffc2d2f7f
Vanraj Desai <vanrajdesai1475@gmail.com>
2025-03-19 23:09:22 +0530

    three

---
+++ src/constants/index.js
@@ -0,0 +1,288 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Home',
+    href: '#home',
+  },
+  {
+    id: 2,
+    name: 'About',
+    href: '#about',
+  },
+  {
+    id: 3,
+    name: 'Work',
+    href: '#work',
+  },
+  {
+    id: 4,
+    name: 'Contact',
+    href: '#contact',
+  },
+];
+
+export const clientReviews = [
+  {
+    id: 1,
+    name: 'Dr. Deepika Patil',
+    position: 'Head Of Deapartment',
+    img: 'assets/review1.png',
+    review:
+      'Rakhi’s attention to detail and problem-solving skills in AI-based projects are outstanding. Her work on the Fake News Detection model showcased deep technical knowledge and creativity.',
+  },
+  {
+    id: 2,
+    name: 'Nilesh Sabnis ',
+    position: 'NSS Camp Teacher',
+    img: 'assets/review2.png',
+    review:
+      'Rakhi has shown exceptional dedication and leadership during the NSS camp. Her ability to organize community activities, motivate fellow volunteers, and contribute to social causes is truly commendable. She actively participated in awareness programs, tree plantation drives, and rural development initiatives',
+  },
+  {
+    id: 3,
+    name: 'Sakshi Desai',
+    position: 'Classmate',
+    img: 'assets/review3.png',
+    review:
+      'Working with Rakhi on our Customer Churn Prediction project was an amazing experience. Her ability to analyze data and optimize models helped us improve accuracy significantly.',
+  },
+  {
+    id: 4,
+    name: 'Vanraj Desai',
+    position: 'Classmate',
+    img: 'assets/review4.png',
+    review:
+      'Rakhi is one of the most hardworking and talented individuals I have worked with. Her ability to grasp complex concepts quickly and apply them in real-world projects is impressive. Whether it’s coding, data science, or ethical hacking, she always brings innovative solutions to the table. Her leadership skills, teamwork, and problem-solving mindset make her an inspiration for our peers. I’ve personally learned a lot from her, and she always pushes the team towards excellence',
+  },
+];
+
+export const myProjects = [
+  {
+    title: 'Ted-Talk Recommendation',
+    desc: 'Ted-Talk Recommendation is a web application that uses machine learning to recommend TED talks based on user preferences. It leverages collaborative filtering and content-based filtering to provide personalized recommendations.',
+    subdesc:
+      'Built with  Python, Tensorflow and Numpy ; Ted-Talk Recommendation offers a seamless user experience, allowing users to discover new talks and expand their knowledge.',
+    href: 'https://github.com/Raj4884/Ted-Talk-recommendation',
+    texture: '/textures/project/project1.mp4',
+    logo: '/assets/project-logo1.png',
+    logoStyle: {
+      backgroundColor: '#2A1816',
+      border: '0.2px solid #36201D',
+      boxShadow: '0px 0px 60px 0px #AA3C304D',
+    },
+    spotlight: '/assets/spotlight1.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'Tensorflow',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Fake News Detection',
+    desc: 'The Fake News Detection System is an AI-driven model designed to classify news articles as real or fake using Natural Language Processing (NLP) and Machine Learning.',
+    subdesc:
+      'With the rise of misinformation and digital propaganda, this project aims to provide a reliable solution for fact-checking and preventing the spread of false information.',
+    href: 'https://github.com/Raj4884/NLP/blob/main/project.ipynb',
+    texture: '/textures/project/project2.mp4',
+    logo: '/assets/project-logo2.png',
+    logoStyle: {
+      backgroundColor: '#13202F',
+      border: '0.2px solid #17293E',
+      boxShadow: '0px 0px 60px 0px #2F6DB54D',
+    },
+    spotlight: '/assets/spotlight2.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Health Care Prediction System',
+    desc: 'The Healthcare Prediction System is an AI-powered solution designed to analyze medical data, predict diseases, and assist in early diagnosis.',
+    subdesc:
+      'Using Machine Learning and Deep Learning, this system helps healthcare professionals and individuals make informed decisions by identifying potential health risks based on symptoms, medical history, and patient data.',
+    href: 'https://github.com/Raj4884/health-care-prediction-by-using-random-forest-model-of-machine-learning/blob/main/Untitled3.ipynb',
+    texture: '/textures/project/project3.mp4',
+    logo: '/assets/project-logo3.png',
+    logoStyle: {
+      backgroundColor: '#60f5a1',
+      background:
+        'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+      border: '0.2px solid rgba(208, 213, 221, 1)',
+      boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+    },
+    spotlight: '/assets/spotlight3.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Hate Speech Analysis',
+    desc: 'The Hate Speech Analysis & Detection System is an AI-powered solution that identifies and classifies offensive, abusive, or hateful content in text-based data',
+    subdesc:
+      ' Using Natural Language Processing (NLP) and Deep Learning, the system helps in monitoring and preventing cyberbullying, online harassment, and hate speech propagation across digital platforms.',
+    href: 'https://github.com/Raj4884/Hate-Speech-Analysis-',
+    texture: '/textures/project/project4.mp4',
+    logo: '/assets/project-logo4.png',
+    logoStyle: {
+      backgroundColor: '#0E1F38',
+      border: '0.2px solid #0E2D58',
+      boxShadow: '0px 0px 60px 0px #2F67B64D',
+    },
+    spotlight: '/assets/spotlight4.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Weather Forecasting',
+    desc: 'The Weather Forecasting System is an  model designed to predict temperature, rainfall, humidity, and other atmospheric conditions using historical weather data, real-time sensor inputs, and machine learning techniques.',
+    subdesc:
+      'is system aims to provide accurate forecasts for various applications, including disaster management, agriculture, transportation, and urban planning.',
+    href: 'https://github.com/Raj4884/weather/blob/main/weather.ipynb',
+    texture: '/textures/project/project5.mp4',
+    logo: '/assets/project-logo5.png',
+    logoStyle: {
+      backgroundColor: '#1C1A43',
+      border: '0.2px solid #252262',
+      boxShadow: '0px 0px 60px 0px #635BFF4D',
+    },
+    spotlight: '/assets/spotlight5.png',
+    tags: [
+      {
+        id: 1,
+        name: 'Tensorflow',
+        path: '/assets/react.png',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: 'NSS Camp',
+    pos: 'Coordinator',
+    duration: '2025 - Present',
+    title: "Led and participated in community service initiatives, tree plantation drives, and social awareness campaigns.",
+    icon: '/assets/framer(2).png',
+    animation: 'victory',
+  },
+  {
+    id: 2,
+    name: 'Pera',
+    pos: 'Volunteer',
+    duration: '2024 - 2025',
+    title: "Actively volunteered in organizing and managing football tournaments, inter-college sports meets, and community events.",
+    icon: '/assets/figma.png',
+    animation: 'clapping',
+  },
+  {
+    id: 3,
+    name: 'Cubicles',
+    pos: 'Member',
+    duration: '2024 - 2025',
+    title: "Developed and optimized Cubicles Git, a scalable distributed version control system designed for seamless team collaboration.",
+    icon: '/assets/notion.png',
+    animation: 'salute',
+  },
+];
